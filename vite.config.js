d1d43dd88a93fa503a42b5b7a69f52c22d6705c0
Marcelo Macedo <marcelo3macedo@gmail.com>
2025-05-26 14:17:17 -0300

    feat: adding base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jogo_soma_dedinhos/',
   plugins: [react(),
     tailwindcss()],
 })
